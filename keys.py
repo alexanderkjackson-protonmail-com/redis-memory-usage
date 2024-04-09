8aef5557ee78d98461a75c0d51e76ad4a0756b22
alexanderkjackson <alexanderkjackson@protonmail.com>
2024-04-09 00:11:05 +0000

    Add argparser and heap_size argument with default at 10.

--- keys.py
+++ keys.py
@@ -2,12 +2,19 @@ import redis
 import heapq
 import argparse
 
+# Collect argument
+parser = argparse.ArgumentParser(description='Retrieves top memory-consuming\
+                                 keys from Redis database.')
+parser.add_argument('--heap-size', type=int, default=10, help='Specify number\
+                    of keys to retrieve (top heap_size keys by size).')
+args = parser.parse_args()
+
 # Connect to Redis
 r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 # Batch size for pipelining
 BATCH_SIZE = 10
-heap_size = 10
+heap_size = args.heap_size
 
 # Function to process a batch of keys with MEMORY USAGE using pipelining
 def process_keys(keys):
